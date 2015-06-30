0ffa13450d870214b033497b697a8fb1f0efe9c2
Ari Nadrich <anadrich@gmail.com>
2015-06-30 13:52:33 -0400

    SSLv1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ActiveRecord::Base
 	before_save { self.email = email.downcase }
-	validates( :name, presence: true, length: {maximum: 50})
+	validates :name, presence: true, length: {maximum: 50}
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validates( :email, presence: true, format: {with: VALID_EMAIL_REGEX},
-		uniqueness: { case_sensitive: false })
+	validates :email, presence: true, format: {with: VALID_EMAIL_REGEX},
+		uniqueness: { case_sensitive: false }
 	has_secure_password
 	validates :password, presence: true, length: { minimum: 6 }
+	VALID_NUMBER_REGEX = /(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]‌​)\s*)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)([2-9]1[02-9]‌​|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})/
+	validates :number, presence:true, format: {with: VALID_NUMBER_REGEX}
 end
