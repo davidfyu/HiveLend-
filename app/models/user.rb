b5e46ee93682e274e62099472db90baa975e03a0
Ari Nadrich <anadrich@gmail.com>
2015-07-08 12:15:01 -0400

    password reset part 1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-	attr_accessor :remember_token
+	
+  attr_accessor :remember_token, :reset_token
 
 	before_save { self.email = email.downcase }
 	validates :name, presence: true, length: {maximum: 50}
@@ -40,4 +41,17 @@ class User < ActiveRecord::Base
   def forget
     update_attribute(:remember_digest, nil)
   end
+
+    # Sets the password reset attributes.
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # Sends password reset email.
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150707170853) do
+ActiveRecord::Schema.define(version: 20150708155256) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20150707170853) do
     t.string   "number_of_hives"
     t.string   "years_exp"
     t.string   "price_per_hive"
+    t.string   "reset_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
