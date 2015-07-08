d794fac0ed7542f2f691222e7e3b1a0aa719ca37
Ari Nadrich <anadrich@gmail.com>
2015-07-08 14:14:32 -0400

    pw reset attempt 3

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150708155256) do
+ActiveRecord::Schema.define(version: 20150708180249) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20150708155256) do
     t.string   "years_exp"
     t.string   "price_per_hive"
     t.string   "reset_digest"
+    t.datetime "reset_sent_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
